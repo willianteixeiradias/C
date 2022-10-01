55e8797b9aa01c5bb241cdb490f125776095e09a
Willian Teixeira <112757886+willianteixeiradias@users.noreply.github.com>
2022-10-01 12:03:02 +0000

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char** argv) {
+	char nome [30];
+	char endereco [30];
+	int idade ;
+	printf("digite seu nome \n");
+	scanf("%s", &nome);
+	printf("digite seu endereco \n");
+	scanf("%s" , &endereco);
+	printf("digite a sua idade");
+	scanf("%d", & idade);
+	
+	printf ("\n nome : %s", nome);
+	printf ("\n nome : %s", endereco);
+	printf ("\n nome : %d", idade);
+	return 0;
+}
